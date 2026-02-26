1c768ab08b7886b8e1c105be132ba579c5787b4c
VINICIUS MANFRIN ZAMARA <26458@unifacef.edu.br>
2026-02-26 20:28:44 -0300

    Aula02: Métodos Void e Métodos de Construção

--- ClasseObjeto/src/Carro.java
+++ ClasseObjeto/src/Carro.java
@@ -12,8 +12,59 @@ public class Carro{
         // this representa um objeto que chama o método
         return " Marca: " + this.marca +
                 " Modelo: " + this.modelo +
-                " Velocidade " + this.velocidade +
+                " Velocidade: " + this.velocidade +
                 " Motor: " + this.motor;
+    }
+
+    // Método contrutor (deve ter o mesmo nome da classe)
+    public Carro(String marca, String modelo, float velocidade, boolean motor){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidade = velocidade;
+        this.motor = motor;
+    }
+
+    // Método Ligar Motor
+    // É necessário colocar o void nesse método, pois o mesmo não retorna um valor
+    public void ligar(){
+        if (!this.motor){
+            this.motor = true;
+            System.out.println("Motor Ligado!");
+        }
+        else{
+            System.out.println("O motor já está ligado!");
+        }
+    }
+
+    public void desligar(){
+        if (this.motor){
+            this.motor = false;
+            System.out.println("Motor Desligado!");
+        }
+        else{
+            System.out.println("O motor já está desligado!");
+        }
+    }
+
+    // Acelerar carro em um valor x
+    public void acelerar(float x){
+        if (this.motor){
+            this.velocidade += x;
+            System.out.println("O carro acelerou, nova velocidade: " + this.velocidade);
+        }
+        else{
+            System.out.println("O carro precisa estar ligado para acelerar");
+        }
+    }
+
+    public void frear(float x){
+        if (this.motor && this.velocidade - x >= 0){
+            this.velocidade -= x;
+            System.out.println("O carro freou, nova velocidade: " + this.velocidade);
+        }
+        else{
+            System.out.println("O carro precisa estar ligado para frear");
+        }
     }
 }
 
--- ClasseObjeto/src/TestaCarro.java
+++ ClasseObjeto/src/TestaCarro.java
@@ -4,22 +4,30 @@
 
 
 public class TestaCarro {
-    public void main(String[] args){
-        // Instânciar objeto obj1
-        Carro obj1 = new Carro();
-        obj1.marca = "Fiat";
-        obj1.modelo = "Uno";
-        obj1.velocidade = 0;
-        obj1.motor = false;
-        System.out.println(obj1); // Imprime o endereço de memória
+    public static void main(String[] args){
+        // Instânciar objeto objeto 1
+        Carro obj1 = new Carro("Fiat", "Uno", 0, false);
         System.out.println(obj1.toString()); // Chama a função toString()
-        Carro obj2 = new Carro();
-        obj2.marca = "GM";
-        obj2.modelo = "Onix";
-        obj2.velocidade = 0;
-        obj2.motor = false;
-        System.out.println(obj2);
+        obj1.desligar();
+        System.out.println(obj1.toString());
+        obj1.ligar();
+        System.out.println(obj1.toString());
+
+        // Instância objeto 2 com um método construtor
+        Carro obj2 = new Carro("GM", "Onix", 27, true);
         System.out.println(obj2.toString());
+        obj2.ligar();
+        System.out.println(obj2.toString());
+        obj2.desligar();
+        System.out.println(obj2.toString());
+
+
+        obj2.desligar();
+
+        obj1.acelerar(60);
+        obj1.acelerar(60);
+        obj1.frear(60);
+        obj1.frear(60);
     }
 }
 
