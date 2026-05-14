0129ee8897dc41d3499d87da25716f8405bee45c
VINICIUS MANFRIN ZAMARA <26458@unifacef.edu.br>
2026-05-14 20:44:05 -0300

    Inicialização de projeto com Hibernate

---
+++ Aulas/API/bd/src/main/java/dc/unifacef/bd/model/Produto.java
@@ -0,0 +1,18 @@
+package dc.unifacef.bd.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="produtos")
+public class Produto {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private String nome;
+    @Column (nullable = false)
+    private Double preco;
+    @Column (length = 500)
+    private String descricao;
+}
